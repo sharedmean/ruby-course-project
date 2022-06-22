f388b83d070753726b7d029592003b057dcfe7ea
Julia Skorobogatova <skor.julia@bk.ru>
2022-06-22 16:10:19 +0300

    some fixes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   validates :title, presence: true, length: {minimum: 2} #  presence: true means that validates automatically 
   validates :picture, presence: true
-  after_commit :post_created_email, on: :create  
-  after_commit :post_updated_email, on: :update 
+  #after_commit :post_created_email, on: :create  
+  #after_commit :post_updated_email, on: :update 
   mount_uploader :picture, PictureUploader
   
   def author?(user)
